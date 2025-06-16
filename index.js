9283f30e7ab037993000c3b824c7617391d97c63
d.kostenko <d.kostenko@bp-solutions.ru>
2025-06-16 12:26:03 +0300

    init

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(123);
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Express работает на Render!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
